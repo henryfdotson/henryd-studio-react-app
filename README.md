29cd181587f610751d8dfd1e3a11b3ff0e129b91
Henry F. Dotson <henryfdotson@gmail.com>
2023-01-02 11:22:53 -0500

    Adding basic React page routing functionality. Added pages.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-## Hello! I'm re-creating my self-hosted portfolio website as a React.JS single page application.
+# Hello! I'm re-creating my self-hosted portfolio website as a React.JS single page application.
+
+## Structure of app
+
+This is a very simple portfolio web app based off of 4 pages
+
+- Homepage: Displaying the current date and a random photograph.
+- About: A simple bio.
+- Resume: An embedded resume.
+- Contact: A contact page containing links.
--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
