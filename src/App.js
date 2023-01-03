c95cf75a1d4a03fc7d5dabbd3b992e4acdf2696d
Henry F. Dotson <henryfdotson@gmail.com>
2023-01-02 20:11:22 -0500

    Added Dark Mode functionality. Added Grid/Content to Contact page.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,44 @@ import Navigation from './components/Navigation';
 import About from './components/About';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
-import React from "react";
+import React, { useEffect, useState } from "react";
+// Enable Routing
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from "react-router-dom";
+// Import Boostrap Grid
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// Import Dark Mode CSS
+import './darkmode.css'
 
 function App() {
+
+  // Dark Mode Enable
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
+    <div className={`App ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
+      <h1>Hello, world!</h1>
       <Router>
         <div>
           <Navigation />
+
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
