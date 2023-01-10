3d5837cbd77cf2efaf932bff9edefe2d9a714a7e
Henry F. Dotson <henryfdotson@gmail.com>
2023-01-09 21:58:36 -0500

    Adding build, updated favicon.

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ function About(){
            <img src={bioPic} height={20} className='img-thumbnail' alt="An image of Henry F. Dotson" />
         </Col>
         <Col md>
-          <p className="about-text">Henry Dotson is a Philadelphia-based artist and System Administrator working at Girard College. At Girard, he is responsible for maintaining the school's network systems and ensuring that all students and staff have access to reliable technology. In addition to this, Henry has completed a four-year project called "Art Every Day" and is currently developing a video game titled "Ghosted". In the future, he hopes to combine his love of art and computer systems in whatever form that may take.</p>
+          <p className="about-text">Henry F. Dotson is a Philadelphia-based artist and System Administrator working at Girard College. At Girard, he is responsible for maintaining the school's network systems and ensuring that all students and staff have access to reliable technology. In addition to this, Henry has completed a four-year project called "Art Every Day" and is currently developing a video game titled "Ghosted". In the future, he hopes to combine his love of art and computer systems in whatever form that may take.</p>
         </Col>
       </Row>
     </Container>
